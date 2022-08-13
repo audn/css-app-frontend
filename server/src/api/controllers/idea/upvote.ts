a9c2e8b568e850f5c4a2bd7814fff2ac237e13ea
Audun Hilden <audunhilden@live.no>
2022-08-13 06:20:04 +0200

    fix<upvote>: upvotes would delete other upvotes

--- server/src/api/controllers/idea/upvote.ts
+++ server/src/api/controllers/idea/upvote.ts
@@ -4,50 +4,51 @@ import { GetIdea } from '../../../lib/schema/idea';
 import { APIJson } from '../../../lib/types';
 
 export const upvote = async (req: Request<GetIdea['params']>, res: APIJson) => {
-    const userId = res.locals.userId;
-    const ideaId = req.params.id;
+      const userId = res.locals.userId;
+      const ideaId = req.params.id;
 
-    const idea = await prisma.idea.findUnique({
-        where: {
-            id: ideaId,
-        },
-        include: {
-            user: true,
-            upvotes: true,
-        },
-    });
-
-    if (!idea) return res.status(404).json({ error: 'Not found' });
-
-    const hasPreviouslyUpvoted = await prisma.upvote.findFirst({
-        where: {
-            ideaId,
-        },
-    });
-
-    if (hasPreviouslyUpvoted) {
-        await prisma.upvote.delete({
+      const idea = await prisma.idea.findUnique({
             where: {
-                id: hasPreviouslyUpvoted.id,
+                  id: ideaId,
             },
-        });
-        return res.json({ message: 'Unvoted post' });
-    } else {
-        await prisma.upvote.create({
-            data: {
-                idea: {
-                    connect: {
-                        id: req.params.id,
-                    },
-                },
-                user: {
-                    connect: {
-                        id: userId,
-                    },
-                },
+            include: {
+                  user: true,
+                  upvotes: true,
             },
-        });
-        return res.json({ payload: { results: idea } });
-    }
+      });
+
+      if (!idea) return res.status(404).json({ error: 'Not found' });
+
+      const hasPreviouslyUpvoted = await prisma.upvote.findFirst({
+            where: {
+                  ideaId,
+                  userId,
+            },
+      });
+
+      if (hasPreviouslyUpvoted) {
+            await prisma.upvote.delete({
+                  where: {
+                        id: hasPreviouslyUpvoted.id,
+                  },
+            });
+            return res.json({ message: 'Unvoted post' });
+      } else {
+            await prisma.upvote.create({
+                  data: {
+                        idea: {
+                              connect: {
+                                    id: req.params.id,
+                              },
+                        },
+                        user: {
+                              connect: {
+                                    id: userId,
+                              },
+                        },
+                  },
+            });
+            return res.json({ payload: { results: idea } });
+      }
 };
 export default upvote;
