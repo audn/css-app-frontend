2db30f8553a451205aeb0552d2f66fe6e4b06a21
Audun Hilden <audunhilden@live.no>
2022-08-13 07:41:15 +0200

    refactor<Charlie>: added className prop

--- client/src/common/pages/home/screens/Login.tsx
+++ client/src/common/pages/home/screens/Login.tsx
@@ -1,17 +1,17 @@
 import { Button } from '../../../components/Buttons';
 import Animate from '../../../components/layout/Animate';
-import Bravo from '../../../components/layout/headings/Bravo';
-import P from '../../../components/layout/headings/P';
+import Alpha from '../../../components/layout/headings/Alpha';
+import Charlie from '../../../components/layout/headings/Charlie';
 import { fadeIn } from '../../../utils/data/animations';
 
 function Login() {
   return (
     <Animate variants={fadeIn} className="flex flex-col items-center ">
-      <div className="flex flex-col items-center p-8 px-12 rounded-2xl bg-types-100">
-        <Bravo>Let the brainstorming begin!</Bravo>
-        <P className="mb-8 text-lg text-on-150">
+      <div className="flex flex-col items-center p-6 text-center sm:p-8 sm:px-12 rounded-2xl">
+        <Alpha>Brainstorm ideas!</Alpha>
+        <Charlie className="mb-8 !font-medium !text-lg text-on-100">
           Do you have an idea of what css.app can be built into?
-        </P>
+        </Charlie>
         <Button.TwitterAuth />
       </div>
     </Animate>
