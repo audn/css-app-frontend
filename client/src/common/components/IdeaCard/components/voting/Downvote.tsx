7212486d13b74342e3f4f0e87668765703d6d7ba
Audun Hilden <audunhilden@live.no>
2022-08-14 07:11:05 +0200

    refactor<IdeaCard>: added active class on vote icons

--- client/src/common/components/IdeaCard/components/voting/Downvote.tsx
+++ client/src/common/components/IdeaCard/components/voting/Downvote.tsx
@@ -1,6 +1,7 @@
 import { SyntheticEvent } from 'react';
 import Tooltip from '../../../../layouts/Tooltip';
 import { fadeIn } from '../../../../utils/data/animations';
+import concat from '../../../../utils/helpers/concat';
 import Animate from '../../../layout/Animate';
 
 function Downvote({
@@ -15,7 +16,10 @@ function Downvote({
       <Animate
         variants={fadeIn}
         onClick={(e) => onClick(e as SyntheticEvent)}
-        className="flex items-center justify-center rounded-full w-7 h-7 hover:bg-types-200"
+        className={concat(
+          active ? 'bg-types-200 text-indigo-500' : 'hover:bg-types-200',
+          'flex items-center justify-center rounded-full w-7 h-7 ',
+        )}
       >
         <i className="text-sm sm:text-lg fa-solid fa-caret-down !leading-0" />
       </Animate>
