49dd85c5e6aabd5d7360254ab28a9a9787722538
Audun Hilden <audunhilden@live.no>
2022-09-03 02:56:28 +0200

    refactor: added skeleton loading

--- client/src/common/utils/hydration/Ideas/index.tsx
+++ client/src/common/utils/hydration/Ideas/index.tsx
@@ -1,9 +1,11 @@
 import { AnimatePresence } from 'framer-motion';
+import { SkeletonTheme } from 'react-loading-skeleton';
 import IdeaCard from '../../../components/IdeaCard';
 import Animate from '../../../components/layout/Animate';
 import Error from '../../../components/layout/Broadcast/Error';
 import { Hydration } from '../../../lib/interfaces';
 import { fadeIn } from '../../data/animations';
+import SkeletonIdeaCard from './components/Skeleton';
 
 function IdeasHydration({
   data,
@@ -12,8 +14,14 @@ function IdeasHydration({
   refetch,
   isRefetching,
 }: Hydration.Ideas) {
-  if (isLoading) {
-    return <>loading..</>;
+  if (!isLoading) {
+    return (
+      <SkeletonTheme color="#272e39" highlightColor="#38414f">
+        <Animate variants={fadeIn} className="grid grid-cols-1 gap-5">
+          {[...Array.from(Array(20)).map((x) => <SkeletonIdeaCard />)]}
+        </Animate>
+      </SkeletonTheme>
+    );
   } else if (error) {
     return (
       <Error
