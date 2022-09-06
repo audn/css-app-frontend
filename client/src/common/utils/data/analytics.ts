7945a00993927aaa80c03228a4c674b0530323e9
Audun Hilden <audunhilden@live.no>
2022-09-06 06:28:32 +0200

    refactor<GA>: added google analytics code

--- client/src/common/utils/data/analytics.ts
+++ client/src/common/utils/data/analytics.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'UA-REPLACE-ME';
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_PROPERTY_ID as string;
 
 export const trackPageview = (url: string) => {
   try {
@@ -19,7 +19,7 @@ export const trackEvent = ({
   label: string;
   value: string;
 }) => {
-      try {
+  try {
     window.gtag('event', action, {
       event_category: category,
       event_label: label,
