13b98e70f2c535a18434e756c779734eb2ad9bca
Audun Hilden <audunhilden@live.no>
2022-11-22 05:50:23 +0100

    refactor: initialized template

---
+++ client/tailwind.colors.js
@@ -0,0 +1,25 @@
+/*
+100 = base color / no states
+150 = border
+200 = hover color
+*/
+module.exports = {
+  brand: {
+    primary: {
+      100: '#f35f84',
+      150: '#737ff1',
+    },
+  },
+  types: {
+    50: '#151515',
+    100: '#1d1d1d',
+    150: '#2c2c2c',
+    200: '#302f2f',
+    250: '#3a3a3a',
+  },
+  on: {
+    50: '#969696',
+    100: '#b6b7bd',
+    150: '#eff3f6',
+  },
+};
