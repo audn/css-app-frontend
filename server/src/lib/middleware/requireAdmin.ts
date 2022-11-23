287729d17b56fb21a9ba09700587dda79f8a8017
Audun Hilden <audunhilden@live.no>
2022-11-23 07:00:04 +0100

    api<fix>middleware: validate user for requireAdmin

--- server/src/lib/middleware/requireAdmin.ts
+++ server/src/lib/middleware/requireAdmin.ts
@@ -2,24 +2,24 @@ import { NextFunction, Request, Response } from 'express';
 import prisma from '../prisma';
 
 export const requireAdmin = async (
-      req: Request,
-      res: Response,
-      next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-      const userId = res.locals.userId;
-      if (!userId) res.sendStatus(401);
-      else {
-            const user = await prisma.user.findUnique({
-                  where: {
-                        id: userId,
-                  },
-            });
-            if (!user) return res.sendStatus(401);
+    const userId = req.user?.id;
+    if (!userId) res.sendStatus(401);
+    else {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+        if (!user) return res.sendStatus(401);
 
-            if (user.role == 'ADMIN') {
-                  return next();
-            } else res.sendStatus(401);
-      }
+        if (user.role == 'ADMIN') {
+            return next();
+        } else res.sendStatus(401);
+    }
 };
 
 export default requireAdmin;
