b2959c708c71af0e000c543584502e02c237ee5f
Audun Hilden <audunhilden@live.no>
2022-11-24 04:43:14 +0100

    refactor<hooks>: added all post hooks

--- client/src/common/utils/hooks/api/posts.ts
+++ client/src/common/utils/hooks/api/posts.ts
@@ -1,7 +1,36 @@
 import { API } from '../../../lib/interfaces';
-import { get } from './api';
+import { del, get, post, put } from './api';
 
 export async function getAllPosts(): Promise<API.Response<API.Models.Post[]>> {
   const res = await get('/posts', false);
+  return res.json();
+}
+
+export async function getPostFromId(
+  id: string,
+): Promise<API.Response<API.Models.Post>> {
+  const res = await get(`/posts/${id}`, false);
+  return res.json();
+}
+
+export async function addPost(
+  data: Partial<API.Models.Post>,
+): Promise<API.Response<API.Models.Post>> {
+  const res = await post('/posts', true, data);
+  return res.json();
+}
+
+export async function editPost(
+  id: string,
+  data: Partial<API.Models.Post>,
+): Promise<API.Response<API.Models.Post>> {
+  const res = await put(`/posts/${id}`, true, data);
+  return res.json();
+}
+
+export async function deletePost(
+  id: string,
+): Promise<API.Response<API.Models.Post>> {
+  const res = await del(`/posts/${id}`, true);
   return res.json();
 }
