c15c34470ac833d0d8af31be9d9b807e508fb9ad
Audun Hilden <audunhilden@live.no>
2022-11-24 04:48:52 +0100

    api<refactor>auth: changed return json on error

--- server/src/api/controllers/auth/logout.ts
+++ server/src/api/controllers/auth/logout.ts
@@ -1,12 +1,14 @@
-import { Request, Response } from 'express';
+import { Request } from 'express';
+import { APIJson } from '../../../lib/types/types';
 
-const logout = async (req: Request, res: Response) => {
+const logout = async (req: Request, res: APIJson) => {
     //@ts-ignore
     req.session.destroy((err) => {
         if (err) {
-            return res.json({
-                success: false,
-                message: err.message,
+            console.log(err.message);
+
+            return res.status(400).json({
+                error: err.message,
             });
         } else {
             return res.status(200).json({});
