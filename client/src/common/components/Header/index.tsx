00de08c0e1629304c53fbf608d41c0f2b5b11efb
Audun Hilden <audunhilden@live.no>
2022-11-29 05:09:49 +0100

    refactor<Header>: removed search

--- client/src/common/components/Header/index.tsx
+++ client/src/common/components/Header/index.tsx
@@ -1,34 +1,32 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { Form } from '../Form';
+import { useState } from 'react';
 import Logo from '../misc/Logo';
 import LibrarySelector from './components/LibrarySelector';
 import Navigation from './Navigation';
 
 export const Header = () => {
-  const router = useRouter();
+  //   const router = useRouter();
   const [search, setSearch] = useState<string | undefined>('');
-  // const [pendingTimeout, setTimeoutHandle] = useState<number | null>(null);
+  //   const [pendingTimeout, setTimeoutHandle] = useState<number | null>(null);
 
-  const handleSearchOn = () => {
-    if (search == undefined || search.length <= 1) {
-      setTimeout(() => {}, 1000);
-    } else {
-      //   useMainState.setState({ searchInput: search });
-      void router.push(
-        {
-          pathname: '',
-          query: {
-            // ...(router.pathname !== '/packs' && { bots: ['bots'] }),
-            ...router.query,
-            q: search,
-          },
-        },
-        undefined,
-        { shallow: true },
-      );
-    }
-  };
+  //   const handleSearchOn = () => {
+  //     if (search == undefined || search.length <= 1) {
+  //       setTimeout(() => {}, 1000);
+  //     } else {
+  //       //   useMainState.setState({ searchInput: search });
+  //       void router.push(
+  //         {
+  //           pathname: '',
+  //           query: {
+  //             // ...(router.pathname !== '/packs' && { bots: ['bots'] }),
+  //             ...router.query,
+  //             q: search,
+  //           },
+  //         },
+  //         undefined,
+  //         { shallow: true },
+  //       );
+  //     }
+  //   };
   //   function handleSearch(event: SyntheticEvent) {
   //     event.preventDefault();
   //     if (search && search.length < 1) {
@@ -40,31 +38,31 @@ export const Header = () => {
   //       handleSearchOn();
   //     }
   //   }
-  const handleEmptySearchField = () => {
-    if (search == undefined || search.length == 0) {
-      void router.push({
-        query: {},
-      });
-      return;
-    }
-  };
-  useEffect(() => {
-    // @ts-ignore
-    return setTimeoutHandle((v) => {
-      if (v != null) {
-        clearTimeout(v);
-      }
+  //   const handleEmptySearchField = () => {
+  //     if (search == undefined || search.length == 0) {
+  //       void router.push({
+  //         query: {},
+  //       });
+  //       return;
+  //     }
+  //   };
+  //   useEffect(() => {
+  //     // @ts-ignore
+  //     return setTimeoutHandle((v) => {
+  //       if (v != null) {
+  //         clearTimeout(v);
+  //       }
 
-      return setTimeout(() => {
-        handleEmptySearchField();
-        if (search?.length == 1) {
-          return;
-        }
+  //       return setTimeout(() => {
+  //         handleEmptySearchField();
+  //         if (search?.length == 1) {
+  //           return;
+  //         }
 
-        handleSearchOn();
-      }, 300);
-    });
-  }, [search]);
+  //         handleSearchOn();
+  //       }, 300);
+  //     });
+  //   }, [search]);
   return (
     <header className="fixed top-0 h-[80px] z-50 flex items-center w-full px-6 bg-types-body/20 filter backdrop-blur">
       <div className="container flex items-center justify-between">
@@ -72,7 +70,7 @@ export const Header = () => {
           <Logo />
           <div className="flex items-center ml-5 space-x-3">
             <LibrarySelector />
-            <div className="text-lg font-semibold text-white/30">/</div>
+            {/* <div className="text-lg font-semibold text-white/30">/</div>
             <div className="flex items-center">
               <Form.Input
                 onChange={setSearch}
@@ -82,7 +80,7 @@ export const Header = () => {
                 placeholder="Search..."
                 inputClassName="bg-transparent focus:bg-types-200/80 hover:bg-types-200/80 "
               />
-            </div>
+            </div> */}
           </div>
         </div>
         <Navigation />
