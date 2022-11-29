b6eeee190e79ed5820c53d21684bbe5e032801b9
Audun Hilden <audunhilden@live.no>
2022-11-29 06:42:44 +0100

    refactor<form>: changed color in formlabel

--- client/src/common/components/Form/components/FormLabel.tsx
+++ client/src/common/components/Form/components/FormLabel.tsx
@@ -6,14 +6,12 @@ function FormLabel({ disabled, label, required, description, id }: Form.Label) {
     return (
       <label
         className={concat(
-          disabled ? 'text-on-50' : 'text-on-100',
+          disabled ? 'text-on-50' : 'text-white',
           'block mb-3 font-semibold text-[14px] text-left',
         )}
         htmlFor={id}
       >
-        <span className="flex items-center ">
-          {label} {required && <span className="text-red-500">*</span>}{' '}
-        </span>
+        {label} {required && <span className="text-red-500">*</span>}
         {description && (
           <div className={`!font-normal mt-2 text-on-100`}>{description}</div>
         )}
