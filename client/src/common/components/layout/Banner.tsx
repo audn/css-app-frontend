bcdd93627e17a028dd20debc1f62d21298fd9ea1
Audun Hilden <audunhilden@live.no>
2022-11-30 18:57:40 +0100

    refactor<banner>: responsive

--- client/src/common/components/layout/Banner.tsx
+++ client/src/common/components/layout/Banner.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 function Banner({ text }: { text: string | ReactNode }) {
   return (
     <div className="flex justify-center mb-10 -mt-10">
-      <div className="flex items-center h-10 gap-2 px-4 truncate rounded-lg group bg-types-200">
+      <div className="flex items-center gap-2 px-4 py-3 rounded-lg bg-types-200">
         <div className="flex-none w-3 h-3 bg-orange-500 rounded-full"></div>
         <p className="text-white/80">{text}</p>
       </div>
