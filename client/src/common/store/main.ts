44441cfd20d72bad41eb753207885dc027dbce27
Audun Hilden <audunhilden@live.no>
2022-11-30 16:33:10 +0100

    fix<store>: invalid default store for version

--- client/src/common/store/main.ts
+++ client/src/common/store/main.ts
@@ -7,7 +7,7 @@ type Store = {
 };
 const useMainState = create<Store>(() => ({
   library: 'TailwindCSS',
-  version: 'v.3.2.4',
+  version: 'v3.2.4',
   src: "<script src='https://cdn.tailwindcss.com/3.2.4'></script>",
 }));
 
