157a76c7148e29549e2941f4b79bc5a09fdc1e08
Audun Hilden <audunhilden@live.no>
2022-12-01 14:09:13 +0100

    test<thumb>: save screenshot of post

--- server/src/api/routes/posts.ts
+++ server/src/api/routes/posts.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import requireAuth from '../../lib/middleware/requireAuth';
 import prisma from '../../lib/prisma';
 import { addDummyData } from '../controllers/posts/addDummyData';
+import { renderImage } from '../controllers/posts/thumb/image';
 
 const posts = Router();
 
@@ -17,6 +18,7 @@ posts.get('/delete', async (req: any, res: any) => {
 });
 posts.get('/', allPosts);
 posts.post('/', requireAuth, createPost);
+posts.get('/:id/thumb', renderImage);
 posts.delete('/:id', requireAuth, removePost);
 posts.put('/:id', requireAuth, editPost);
 posts.get('/:id', getPost);
