e10795a01252309128089f74762e42a5d40bd154
Audun Hilden <audunhilden@live.no>
2022-12-05 05:13:42 +0100

    refactor<search>WIP: added searchbar in layout

--- src/common/layouts/FadeLayout.tsx
+++ src/common/layouts/FadeLayout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import { Form } from '../components/Form';
 import Banner from '../components/layout/Banner';
 import H1 from '../components/layout/headings/H1';
 import H3 from '../components/layout/headings/H3';
@@ -9,6 +10,7 @@ type Props = {
   h3: string;
 };
 export const FadedLayout = ({ h1, h3, children }: Props) => {
+  const [search, setSearch] = useState<string | undefined>('');
   return (
     <>
       <div className="flex items-center h-auto p-5 bg-gradient-to-t from-types-150 to-types-body pt-12 pb-14 md:pb-28  max-h-[d300px] flex-col">
@@ -29,6 +31,19 @@ export const FadedLayout = ({ h1, h3, children }: Props) => {
         <div className="flex flex-col items-center max-w-4xl text-center">
           <H1 className="text-white">{h1}</H1>
           <H3 className="mt-3 !font-medium">{h3}</H3>
+        </div>
+        <div className="relative flex items-center w-full max-w-sm mt-12">
+          <span className="absolute text-sm left-5">
+            <i className="fa-regular fa-magnifying-glass" />
+          </span>
+          <Form.Input
+            onChange={setSearch}
+            value={search}
+            autoFocus={false}
+            id="headerSearch"
+            placeholder="Search..."
+            inputClassName="!bg-types-200 px-4 !pl-11 py-3  rounded-full focus:bg-types-200/80 hover:bg-types-200/80 "
+          />
         </div>
       </div>
       <div className="container flex flex-col">
