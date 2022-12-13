f4b23517163a0cb27848b7fa7b7a23651b49b77c
Audun Hilden <audunhilden@live.no>
2022-12-13 05:14:48 +0100

    fix<thumbnail>: max-height to 600px

--- src/common/utils/useGenerateThumbnail.ts
+++ src/common/utils/useGenerateThumbnail.ts
@@ -2,31 +2,34 @@ import html2canvas from 'html2canvas';
 import { uploadThumbnail } from './hooks/api/posts';
 
 export async function useGenerateThumbnail(id: string) {
-  const iframe =
-    typeof window !== 'undefined' &&
-    (document.getElementsByTagName('iframe') as any);
-  const screen = iframe[0]?.contentDocument?.body;
-  html2canvas(screen, {
-    allowTaint: true,
-    useCORS: true,
-    windowWidth: 800,
-    windowHeight: 600,
-  }).then(async (canvas) => {
-    //   scale: 2,
-    const base64image = canvas.toDataURL('image/png');
+  if (typeof window !== 'undefined') {
+    const iframe = document.getElementsByTagName('iframe');
+    const screen = iframe[0]?.contentDocument?.body as HTMLElement;
 
-    const buffer = Buffer.from(
-      base64image.replace(/^data:image\/\w+;base64,/, ''),
-      'base64',
-    );
-    await uploadThumbnail(id, {
-      buffer: buffer,
-      encoding: '7bit',
-      fieldname: 'image',
-      mimetype: 'image/png',
-      originalname: id,
+    html2canvas(screen, {
+      allowTaint: true,
+      useCORS: true,
+      height: 600,
+      //   width: 800,
+      windowWidth: 800,
+      windowHeight: 600,
+    }).then(async (canvas) => {
+      const base64image = canvas.toDataURL('image/png');
+
+      const buffer = Buffer.from(
+        base64image.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+      );
+      document.body.appendChild(canvas);
+      await uploadThumbnail(id, {
+        buffer: buffer,
+        encoding: '7bit',
+        fieldname: 'image',
+        mimetype: 'image/png',
+        originalname: id,
+      });
     });
-  });
+  }
 }
 
 export default useGenerateThumbnail;
