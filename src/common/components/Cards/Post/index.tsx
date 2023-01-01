828b64711ef5df4f5bf9fcec973063b70f0acebb
Audun Hilden <audunhilden@live.no>
2023-01-01 15:02:39 +0100

    refactor<PostCard>: minor UI changes

--- src/common/components/Cards/Post/index.tsx
+++ src/common/components/Cards/Post/index.tsx
@@ -6,25 +6,27 @@ function PostCard({ author, id, title, generatedImage }: API.Models.Post) {
   return (
     <div className="flex flex-col items-start">
       <Link
-        className="relative w-full h-[235px] bg-types-150 rounded-xl overflow-hidden "
+        className="relative w-full h-[235px]  bg-types-150 rounded-md overflow-hidden "
         // href={`/${library.toLowerCase()}/${title
         //   .replaceAll(' ', '-')
         //   .toLowerCase()}`}
         href={`/component/${id}`}
       >
+        {/* <img src={generatedImage} /> */}
         {generatedImage && (
           <Image
             src={generatedImage}
-            layout="responsive"
-            width={400}
-            height={300}
+            layout="fill"
+            // width={280}
+            // height={250}
+            className="scale-150"
           />
         )}
       </Link>
-      <div className="flex flex-col items-start px-2 py-3">
-        <h2 className="text-lg text-white">{title}</h2>
-        <div className="flex items-center mt-2">
-          {/* <img src={author.avatar} className="w-6 h-6 mr-2 rounded-full" /> */}
+      <div className="flex flex-col items-start w-full px-2 py-3">
+        <h2 className="text-base text-white">{title}</h2>
+        <div className="flex items-center mt-1 text-sm">
+          <img src={author.avatar} className="w-6 h-6 mr-2 rounded-full" />
           <Link
             href={`/user/${author.id}`}
             className="hover:text-white animate"
