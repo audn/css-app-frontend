8ee676124ee025b8f1349f61e7ff42f63b3cd725
Audun Hilden <audunhilden@live.no>
2023-01-03 19:23:44 +0100

    fix<LibraryDropdown>: removed version from CSS3 + added lib vectors

--- src/common/components/layout/Pen/components/LibraryDropdown.tsx
+++ src/common/components/layout/Pen/components/LibraryDropdown.tsx
@@ -21,7 +21,10 @@ function LibraryDropdown({
     )[0].versions[0];
 
     update('library', lib);
-    if (latestVersion) {
+
+    if (lib === 'CSS3') {
+      update('libraryVersion', '');
+    } else if (latestVersion) {
       update('libraryVersion', latestVersion);
     }
   }
@@ -33,9 +36,9 @@ function LibraryDropdown({
 
   return (
     <Dropdown
-      className="py-[0.4em]"
+      className="py-[0.4em] hover:text-white animate"
       isLoading={isLoading}
-      options={{ animateCaret: true, box: true, caret: true }}
+      options={{ animateCaret: true, box: false, caret: true }}
       component={
         <div className="flex flex-col p-1">
           <span className="mb-2 text-sm">Library</span>
@@ -46,33 +49,40 @@ function LibraryDropdown({
             onChange={(event: ChangeEvent<HTMLSelectElement>) =>
               changeLib(event)
             }
-            className="px-3 py-2 text-sm bg-types-250 focus:outline-none"
+            className="px-3 py-2 text-sm bg-types-150/80 focus:outline-none"
           >
             {libs?.payload?.results.map((x) => (
               <option value={x.label}>{x.label}</option>
             ))}
           </select>
-          <span className="mt-2 mb-2 text-sm">Version</span>
-          <select
-            id="version"
-            value={data.libraryVersion}
-            name="version"
-            onChange={(event: ChangeEvent<HTMLSelectElement>) =>
-              changeVersion(event)
-            }
-            className="px-3 py-2 text-sm bg-types-250 focus:outline-none"
-          >
-            {libs?.payload?.results
-              .filter((x) => x.label === useLibraryLabel(data.library))
-              .map((x) =>
-                x.versions.map((x) => <option value={x}>v{x}</option>),
-              )}
-          </select>
+          {data.library !== 'CSS3' && (
+            <>
+              <span className="mt-2 mb-2 text-sm">Version</span>
+              <select
+                id="version"
+                value={data.libraryVersion}
+                name="version"
+                onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+                  changeVersion(event)
+                }
+                className="px-3 py-2 text-sm bg-types-150/80 focus:outline-none"
+              >
+                {libs?.payload?.results
+                  .filter((x) => x.label === useLibraryLabel(data.library))
+                  .map((x) =>
+                    x.versions.map((x) => <option value={x}>v{x}</option>),
+                  )}
+              </select>
+            </>
+          )}
         </div>
       }
     >
-      {useLibraryLabel(data.library)}{' '}
-      <span className="ml-2 text-xs">(v{data.libraryVersion})</span>
+      <img src={`/libraries/${data.library}.svg`} className="w-5 h-5 mr-2" />
+      <p className="text-sm">{useLibraryLabel(data.library)} </p>{' '}
+      {data.library !== 'CSS3' && (
+        <span className="ml-2 text-xs">(v{data.libraryVersion})</span>
+      )}{' '}
     </Dropdown>
   );
 }
