cacc8123ee404b16e4c5a123abc65f592780567d
Audun Hilden <audunhilden@live.no>
2023-01-05 10:39:54 +0100

    fix<search>: width bug

--- src/common/components/Sidebar/components/SearchBar.tsx
+++ src/common/components/Sidebar/components/SearchBar.tsx
@@ -5,8 +5,8 @@ function SearchBar() {
         <i className="fa-solid fa-magnifying-glass" />
       </div>
       <input
-        type="text"
-        className="px-6 pl-10 py-[0.5rem] placeholder:text-white/50 focus:outline-none focus:bg-types-150/80 rounded-full bg-types-100"
+        type="search"
+        className="px-6 pl-10 py-[0.5rem] placeholder:text-white/50 focus:outline-none focus:bg-types-150/80 rounded-full bg-types-100 w-full"
         placeholder="Search..."
       />
     </div>
