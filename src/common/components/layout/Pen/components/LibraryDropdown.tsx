50e920690e763223384287650805abb410b1b065
Audun Hilden <audunhilden@live.no>
2023-01-06 12:04:41 +0100

    refactor<LibraryDropdown>: removed version dropdown from CSS

--- src/common/components/layout/Pen/components/LibraryDropdown.tsx
+++ src/common/components/layout/Pen/components/LibraryDropdown.tsx
@@ -33,6 +33,7 @@ function LibraryDropdown({
     const version = event.target.value;
     update('libraryVersion', version);
   }
+  console.log(data.library);
 
   return (
     <Dropdown
@@ -55,7 +56,7 @@ function LibraryDropdown({
               <option value={x.label}>{x.label}</option>
             ))}
           </select>
-          {data.library !== 'CSS3' && (
+          {useLibraryLabel(data.library) !== 'CSS3' && (
             <>
               <span className="mt-2 mb-2 text-sm">Version</span>
               <select
@@ -83,7 +84,7 @@ function LibraryDropdown({
         className="w-5 h-5 mr-2"
       />
       <p className="text-sm">{useLibraryLabel(data.library)} </p>{' '}
-      {data.library !== 'CSS3' && (
+      {useLibraryLabel(data.library) !== 'CSS3' && (
         <span className="ml-2 text-xs">(v{data.libraryVersion})</span>
       )}{' '}
     </Dropdown>
