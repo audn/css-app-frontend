90e0b6db5952ac5652c996542bdaa470fcc0f923
hailer24 <anmolbhutanianmol@gmail.com>
2023-02-09 01:10:03 +0530

    modified Form.TextArea

--- src/common/components/Form/Textarea/index.tsx
+++ src/common/components/Form/Textarea/index.tsx
@@ -24,7 +24,7 @@ function Textarea({
     } else return validateRegex({ value, regex }) as string;
   };
   return (
-    <div className="max-w-full">
+    <div className="max-w-full min-h-fit">
       <div className="flex items-center justify-between">
         {label && (
           <FormLabel
@@ -42,13 +42,14 @@ function Textarea({
         value={value}
         maxLength={maxLength}
         minLength={minLength}
+        cols={3}
         onChange={(event) => onChange(event.target.value)}
         className={concat(
           inputClassName ? inputClassName : '',
           disabled
             ? 'bg-opacity-50 cursor-not-allowed text-on-200 after:!text-on-150'
             : 'focus:bg-types-150/50',
-          'focus:text-white w-full h-full px-4 py-3 transition duration-150 ease-out rounded-lg outline-none scrollbar-none border border-types-150 bg-types-100/50 input-field',
+          'focus:text-white w-full h-28 px-4 py-3 transition duration-150 ease-out rounded-lg outline-none scrollbar-none border border-types-150 bg-types-100/50 input-field box-border resize-none',
         )}
       />
       <FormError label={getErrors()} />
